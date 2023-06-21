68f28a11abde5e34c2d2e0be59aec3432bec9097
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-06-21 15:12:00 -0300

    Criando arquivo com cores padrão

---
+++ src/styles/theme.js
@@ -0,0 +1,38 @@
+export default {
+    COLORS: {
+
+      LIGHT_100: "#FFFFFF",
+      LIGHT_200: "#FFFAF1",
+      LIGHT_300: "#E1E1E6",
+      LIGHT_400: "#C4C4CC",
+      LIGHT_500: "#7C7C8A",
+      LIGHT_600: "#76797B",
+      LIGHT_700: "#4D585E",
+
+      DARK_100: "#000405",
+      DARK_200: "#00070A",
+      DARK_300: "#000204",
+      DARK_400: "#000A0F",
+      DARK_500: "#000C12",
+      DARK_600: "#00111A",
+      DARK_700: "#001119",
+      DARK_800: "#0D161B",
+      DARK_900: "#0D1D25",
+      DARK_1000: "#0D1D25",
+
+      TOMATO_100: "#750310",
+      TOMATO_200: "#92000E",
+      TOMATO_300: "#AB222E",
+      TOMATO_400: "#AB4D55",
+
+      CARROT: "#FBA94C",
+
+      MINT: "#04D361",
+    
+      CAKE_100: "#065E7C",
+      CAKE_200: "#82F3FF",
+
+      GRADIENTE_100: "#82F3FF",
+
+    }
+  }
