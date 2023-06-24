fe8067d492860db190254c94d490f32bb07c0613
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-06-24 16:56:24 -0300

    Mudando cor do background

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    background-color: purple;
+    background-image: ${({ theme }) => theme.COLORS.GRADIENT["100"]};
 `;
