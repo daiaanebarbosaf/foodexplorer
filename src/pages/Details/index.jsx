09c858e95cb5b0aa4873df8f1c9f450cd2368b34
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-06-28 15:05:45 -0300

    Importando o componente Botão

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
-import { Container } from './styles.js'
+import { Container } from './styles.js';
+
+import { Button } from '../../components/Button';
 
 export function Details(){
   return(
     <Container>
       <h1>Hello Word!</h1>
+
+      <Button title="Cadastrar"/>
     </Container>
   )
 }
