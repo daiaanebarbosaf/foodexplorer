14cbba3998ae1ba855a48b1d04022416d44c8670
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-01 10:42:33 -0300

    Definindo o grid da página de Details

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -2,4 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     background-image: ${({ theme }) => theme.COLORS.GRADIENT["100"]};
+
+    width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-rows: 6rem auto;
+    grid-template-areas: 
+    "header"
+    "content";
 `;
