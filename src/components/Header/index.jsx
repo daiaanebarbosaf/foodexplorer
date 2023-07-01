48a3c261e46adf3cd8582daf7d9c0227155e33e1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-01 17:50:15 -0300

    Criando função de expandir menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,21 @@
 import { Container } from './styles';
 import logoAdmin from '../../assets/logo-admin.svg';
 import hiddenMenu from '../../assets/hidden-menu.svg';
+import closeMenu from '../../assets/close-menu.svg';
 
 export function Header(){
+
+    function expandMenu(){
+        hiddenMenu.classList.add('hide');
+        closeMenu.classList.remove('hide');
+    }
+
     return(
         <Container>
-            <img id="hiddenMenu" src={hiddenMenu} alt="Imagem do menu escondido" />
-            <img id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
+            <img className="" id="hiddenMenu" onClick={expandMenu} src={hiddenMenu} alt="Imagem do menu escondido" />
+            <img className="hide" id="closeMenu" src={closeMenu} alt="Imagem para fechar menu" />
+            <img className="" id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
+            
         </Container>
     );
 }
