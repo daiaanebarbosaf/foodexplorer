01902101562dc554b004a938800edfac29cf1562
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-01 10:42:01 -0300

    Adicionando o Header na página de Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from './styles.js';
 
 import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
 
 export function Details(){
   return(
     <Container>
+      <Header />
       <h1>Hello Word!</h1>
 
       <Button title="pedir" />
