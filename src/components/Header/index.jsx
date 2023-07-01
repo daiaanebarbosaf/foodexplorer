7c9cd807dab1f3f9309a74999300eb6bcacd5a40
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-01 17:27:13 -0300

    Adicionado os svgs no Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from './styles';
-import logoAdmin from '../../assets/logo-admin.svg'
+import logoAdmin from '../../assets/logo-admin.svg';
+import hiddenMenu from '../../assets/hidden-menu.svg';
 
 export function Header(){
     return(
         <Container>
-            <img src={logoAdmin} alt="" />
+            <img id="hiddenMenu" src={hiddenMenu} alt="Imagem do menu escondido" />
+            <img id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
         </Container>
     );
 }
