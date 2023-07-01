a6e832d8d26447db9055eb0061d4b7ce585caf99
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-01 10:41:13 -0300

    Criando o estilo do Header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,19 @@
 import styled from 'styled-components';
+
+export const Container = styled.header`
+    grid-area: header;
+
+    height: 6rem;
+    width: 100%;
+
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+
+    display: flex;
+    
+    justify-content: center;
+
+    > img {
+        width: 30%;
+    }
+`;
+
