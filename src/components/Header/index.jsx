3c03142c8095a8bf56d084b89e1d7f942306fd66
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-01 19:05:57 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,44 @@
-import { Container } from './styles';
+import { Container, MenuExpand, MenuClose } from './styles';
+
 import logoAdmin from '../../assets/logo-admin.svg';
-import hiddenMenu from '../../assets/hidden-menu.svg';
-import closeMenu from '../../assets/close-menu.svg';
+import buttonHidden from '../../assets/hidden-menu.svg';
+import buttonClose from '../../assets/close-menu.svg';
 
 export function Header(){
+    const menuExpand = document.getElementById('buttonHidden');
+    const menuClose = document.getElementById('buttonClose');
+    const adminLogo = document.getElementById('logoAdmin');
+    const textMenu = document.getElementById('menuText');
+
 
     function expandMenu(){
-        hiddenMenu.classList.add('hide');
-        closeMenu.classList.remove('hide');
+        menuExpand.classList.add('hide');
+        adminLogo.classList.add('hide');
+
+        menuClose.classList.remove('hide');
+        textMenu.classList.remove('hide');
+        
+    }
+
+    function closeMenu(){
+        menuClose.classList.add('hide');
+        textMenu.classList.add('hide');
+
+        menuExpand.classList.remove('hide');
+        adminLogo.classList.remove('hide');
     }
 
     return(
         <Container>
-            <img className="" id="hiddenMenu" onClick={expandMenu} src={hiddenMenu} alt="Imagem do menu escondido" />
-            <img className="hide" id="closeMenu" src={closeMenu} alt="Imagem para fechar menu" />
-            <img className="" id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
+            <MenuExpand onClick={expandMenu}>
+                <img className="" id="buttonHidden" src={buttonHidden} alt="Imagem do menu escondido" />
+                <img className="" id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
+            </MenuExpand>
+            
+            <MenuClose onClick={closeMenu}>
+                <img className="hide" id="buttonClose" src={buttonClose} alt="Logo da Food Explorer" />
+                <h1 className="hide" id="menuText">Menu</h1>
+            </MenuClose>
             
         </Container>
     );
