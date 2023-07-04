52d905215e2f0d4270f28deb1fa2d7cc357ab6af
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-07-04 15:28:44 -0300

    Reformulando o header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,10 @@
-import { Container, MenuExpand, MenuClose } from './styles';
+import { Container, Menu, MenuExpand, MenuClose } from './styles';
 
 import logoAdmin from '../../assets/logo-admin.svg';
 import buttonHidden from '../../assets/hidden-menu.svg';
 import buttonClose from '../../assets/close-menu.svg';
+
+import { Input } from '../../components/Input';
 
 export function Header(){
 
@@ -37,16 +39,20 @@ export function Header(){
 
     return(
         <Container>
-            <MenuExpand onClick={expandMenu}>
-                <img className="" id="buttonHidden" src={buttonHidden} alt="Imagem do menu escondido" />
-                <img className="" id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
-            </MenuExpand>
-            
-            <MenuClose onClick={closeMenu}>
-                <img className="hide" id="buttonClose" src={buttonClose} alt="Logo da Food Explorer" />
+            <Menu>
+                <MenuExpand onClick={expandMenu}>
+                    <img className="" id="buttonHidden" src={buttonHidden} alt="Imagem do menu escondido" />
+                    <img className="" id="logoAdmin" src={logoAdmin} alt="Logo da Food Explorer" />
+                </MenuExpand>
+                
+                <MenuClose onClick={closeMenu}>
+                    <img className="hide" id="buttonClose" src={buttonClose} alt="Imagem de um X para fechar o menu" />
+                </MenuClose>
+
                 <h1 className="hide" id="menuText">Menu</h1>
-            </MenuClose>
-            
+                <Input placeholder="Busque por pratos ou ingredientes" />
+                <p>Sair</p>
+            </Menu>
         </Container>
     );
 }
