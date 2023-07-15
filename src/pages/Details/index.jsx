58bf9c9e0b9201310c0e9e037ccd3d36448024c8
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-15 13:14:56 -0300

    Adicionando o footer na página principal

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,13 +2,14 @@ import { Container } from './styles.js';
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
 export function Details(){
   return(
     <Container>
       <Header />
 
-      
+      <Footer/>
     </Container>
   )
 }
