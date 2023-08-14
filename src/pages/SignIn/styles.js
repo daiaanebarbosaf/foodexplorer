e275e323e8db1dc823fa732068b5ae382e6208f6
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-14 10:13:35 -0300

    Estilo geral da página e do forms

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,8 +1,27 @@
 import styled from "styled-components";
 
-export const Container = styled.view`
+export const Container = styled.div`
     height: 100vh;
 
     display: flex;
-    align-items: stretch;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    > img {
+        cursor: pointer;
+        width: clamp(15vw, 10em, 30vw);
+        margin-bottom: 4.5625rem;
+    }
+
+    > p {
+        font-size: ;
+    }
+
 `;
