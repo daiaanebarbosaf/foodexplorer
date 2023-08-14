2eac932e08910e2e466b1fde38e74c40d95157e8
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-14 10:13:15 -0300

    Primeiras configurações da página de SingIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,33 @@
-import { Container } from './styles';
+import { Container, Form } from './styles';
+import { FiMail, FiLock } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
 
 export function SignIn(){
     return (
         <Container>
 
+            <Form>
+                <img src={logo} alt="Logo da Food Explorer" />
+
+                <p>Email</p>
+                <Input
+                    type="text"
+                    placeholder="Exemplo: exemplo@exemplo.com.br ou ingredientes" 
+                    icon={FiMail}
+                />
+                <p>Senha</p>
+                <Input
+                    type="password"
+                    placeholder="No mínimo 6 caracteres" 
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar"/>
+            </Form>
         </Container>
     );
 }
