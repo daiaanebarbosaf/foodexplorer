497f6e7aa649d3d756c80f40fd2bd76f6f30f391
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-18 07:59:07 -0300

    Estilo da página SignUp

---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+
+    > header {
+        display: flex;
+        justify-content: center;
+
+        > img {
+            cursor: pointer;
+            width: clamp(15vw, 20em, 130vw);
+            margin-bottom: 3.5625rem;
+        }
+
+    }
+
+    > p {
+        font-family: 'Roboto', sans-serif;
+        font-size: 0.8rem;
+        font-weight: 400;
+
+        color: ${({ theme }) => theme.COLORS.LIGHT_400};
+
+        margin-bottom: 0.3rem;
+    }
+
+    .inputSignUpP {
+        margin-top: 1.5rem;
+    }
+
+    .inputSignUp::placeholder {
+        font-size: 0.7rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT_500};
+    }
+
+    > footer {
+        display: flex;
+        justify-content: center;
+
+        > a {
+
+            margin-top: 1.5rem;
+            color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+            font-family: 'Poppins', sans-serif;
+            font-weight: 500;
+        }
+    } 
+`;
