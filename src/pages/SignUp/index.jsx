b2039219c1234b5002bf87f54010278845adde1b
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-18 07:58:50 -0300

    Criando a página de SignUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,49 @@
+import { Container, Form } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+
+export function SignUp(){
+    return (
+        <Container>
+
+            <Form>
+                <header>
+                    <img src={logo} alt="Logo da Food Explorer" />
+                </header>
+
+                <p>Seu nome</p>
+                <Input
+                    className="inputSignUp"
+                    type="text"
+                    placeholder="Exemplo: Maria da Silva" 
+                />
+
+                <p className="inputSignUpP">Email</p>
+                <Input
+                    className="inputSignUp"
+                    type="text"
+                    placeholder="Exemplo: exemplo@exemplo.com.br" 
+                />
+                <p className="inputSignUpP">Senha</p>
+                <Input
+                    className="inputSignUp"
+                    type="password"
+                    placeholder="No mínimo 6 caracteres" 
+                   
+                />
+
+                <Button title="Criar conta"/>
+
+                <footer>
+                    <a href="#">
+                        Já tenho uma conta                   
+                    </a>
+                </footer>
+
+            </Form>
+        </Container>
+    );
+}
