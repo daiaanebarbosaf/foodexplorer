147a9b8e0c68a042ad6848e98e221f21e21b579d
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-19 10:20:19 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
-import { SignUp } from './pages/SignUp';
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignUp />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
