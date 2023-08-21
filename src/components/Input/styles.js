d78660ce53f4b0e82d1b6fe213df1441c9377637
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-21 08:37:03 -0300

    Ajustando espaçamentos laterais das opções do menu

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -4,6 +4,10 @@ export const Container = styled.div`
     width: 100%;
     display: flex;
     align-items: center;
+    justify-content: center;
+
+    margin-right: 4rem;
+    gap: 1rem;
 
     background-color: ${({ theme }) => theme.COLORS.DARK_900};
     color: ${({ theme }) => theme.COLORS.LIGHT_500};
@@ -11,24 +15,27 @@ export const Container = styled.div`
     border-radius: 5px;
 
     > input {
+   
        height: 2rem;
        width: 100%;
 
-       padding: 1rem;
 
        color: ${({ theme }) => theme.COLORS.LIGHT_100};
        background: transparent;
        border: 0;
 
        font-family: 'Roboto', sans-serif;
-        font-weight: 400;
+          font-weight: 400;
+          font-size: 0.7rem;
 
        &placeholder {
             color: ${({ theme }) => theme.COLORS.LIGHT_500};
+            
        }
     }
 
     > svg {
-         margin-left: 1rem;
+         margin-left: 0.5rem;
+         width: 1rem;
     }
 `;
