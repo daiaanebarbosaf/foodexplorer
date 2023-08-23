acd5671af3018457b67e0296796e0ed3bdafe9c4
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-23 09:29:07 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,19 @@
-import { Container } from './styles';
+import { Container, Banner } from './styles';
+
+import unparalleledFlavors from '../../assets/unparalleled-flavors.png';
+
 import { Header } from '../../components/Header';
 
 export function Home(){
     return(
         <Container>
             <Header />
+
+            <Banner className="gradient-container">
+                <img src={unparalleledFlavors} alt="Quitutes ao ar" />
+                <h1>Sabores inigualáveis</h1>
+                <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
+            </Banner>
 
         </Container>
     );
