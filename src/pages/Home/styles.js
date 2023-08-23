ce2f74ec283ef5399e8fd3557f13921e9f745f5c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-23 09:29:34 -0300

    Style da página Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,35 @@
+import React from 'react';
+
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+    gap: 2rem;
+
+    padding: 0 2rem;
+
+`;
+
+export const Banner = styled.div`
+    display: flex;
+    align-content: center;
+
+    background: linear-gradient(to bottom, #091E26, #00131C);
+
+    width: 90%;
+    height: 120px;
+
+    > img {
+        width: 150px;
+        height: 120px;
+
+        position: relative;
+        left: -25px;
+        bottom: -4px;
+    }
 
 `;
