53f396156e28b2bc58477be94014b3e48d2e6aa3
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-02 18:48:26 -0300

    Adicionando o ButtonText na página de Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,18 +1,30 @@
 import { Container } from './styles.js';
 
-import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+
+import { IoIosArrowBack } from 'react-icons/io';
 
 export function Details(){
   return(
     <Container>
       <Header />
 
-      <Section>
+      <IoIosArrowBack/>
+      <ButtonText
+        title="voltar"
+      />
+
+      <Section id="tags">
         <Tag title="alface"/>
+        <Tag title="cebola"/>
+        <Tag title="pão naan"/>
+        <Tag title="pepino"/>
+        <Tag title="tomate"/>
+
       </Section>
 
       <Footer/>
