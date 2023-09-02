548282e87853ad2cd19a6fc2ddee0dd2bf8eab10
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-02 11:09:48 -0300

    Adicionando a seção de Tag na Home

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,11 +3,17 @@ import { Container } from './styles.js';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 export function Details(){
   return(
     <Container>
       <Header />
+
+      <Section>
+        <Tag title="alface"/>
+      </Section>
 
       <Footer/>
     </Container>
