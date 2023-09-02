c001a9087c1e64d70f2d1269be31389cc50a6d72
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-02 11:10:13 -0300

    Atualizando a cor do DARK_1000:

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -18,7 +18,7 @@ export default {
       DARK_700: "#001119",
       DARK_800: "#0D161B",
       DARK_900: "#0D1D25",
-      DARK_1000: "#0D1D25",
+      DARK_1000: "#192227",
 
       GRADIENT: {
         100: "linear-gradient(90deg, rgba(0, 10, 15, 0.272541) 0%, #000A0F 100%)"
