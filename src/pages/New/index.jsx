8fe3a4c25dde0f60de84e85ee4bf075deecd26e4
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-06 09:56:14 -0300

    Adicionando input de imagem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,13 @@
-import { Container, Form } from './styles';
+import { Container, Form, ImgDishes } from './styles';
 
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
 
 import { IoIosArrowBack } from 'react-icons/io';
+import { FiUpload } from 'react-icons/fi';
+
+
 
 export function New(){
     return (
@@ -26,6 +29,20 @@ export function New(){
                     <header>
                         <h1>Novo Prato</h1>
                     </header>
+
+                    <ImgDishes>
+                        <label 
+                            htmlFor="imgDishes"
+                        >
+                            <FiUpload/>
+                            <p>Selecione uma imagem</p>
+
+                            <input 
+                                id="imgDishes" 
+                                type="file" 
+                            />
+                        </label>
+                    </ImgDishes>
 
                     <Input placeholder="Ex.: Salada Ceasar" />
                 </Form>
