94fa6165e62f22a3cced87d9d7bc2833e92fed58
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-07 16:39:27 -0300

    Criando componente de Ingredientes

--- src/components/Ingredients/index.jsx
+++ src/components/Ingredients/index.jsx
@@ -4,13 +4,22 @@ import { FiPlus, FiX } from 'react-icons/fi';
 
 export function Ingredients({ isNew, value, onClick, ...rest}){
     return(
-        <Container>
+        <Container isNew={isNew}>
            <input
                 type="text"
                 value={value}
                 readOnly={!isNew}
                 {...rest}
            /> 
+
+           <button
+                type="button"
+                onClick={onClick}
+                className={isNew ? 'button-delete' : 'button-add'}
+           >
+
+            {isNew ? <FiPlus/> : <FiX/>}
+           </button>
         </Container>
     )
 }
