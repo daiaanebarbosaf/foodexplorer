40b886e00087043403b7146d11371b646b01d86e
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-08 08:13:30 -0300

    Reajustando as tags de ingredientes.

--- src/components/Ingredients/styles.js
+++ src/components/Ingredients/styles.js
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    width: 35%;
+    
     display: flex;
-    align-items: center;
 
     background-color: ${({ theme, isNew }) => isNew ? "transparent" : theme.COLORS.LIGHT_500};
     color: ${({ theme, isNew }) => isNew ? "transparent" : theme.COLORS.LIGHT_100};
 
     border: ${({ theme, isNew }) => isNew ? `1px dashed ${theme.COLORS.LIGHT_500}` : "none"};
-
-    margin-bottom: 8px;
-    border-radius: 10px;
-    padding-right: 16px;
+    
+    border-radius: 8px;
 
     > button {
+        display: flex;
+        align-items: center;
+     
+
         border: none;
         background: none;
+
+        > svg {
+            margin-right: 0.5rem;
+        }
     }
 
     .button-delete {
@@ -27,14 +34,14 @@ export const Container = styled.div`
     }
 
     > input {
+        text-align: center;
+
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: clamp(0.5vw, 1.5em, 30vw);
 
-        height: 56px;
         width: 100%;
-
-        padding: 12px;
+        padding: 0.2rem;
 
         color: ${({ theme }) => theme.COLORS.LIGHT_100};
         background: transparent;
