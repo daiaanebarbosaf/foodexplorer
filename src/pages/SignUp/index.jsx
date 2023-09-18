e46a212727e770cb85c491862df05273e8f5995c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-18 11:15:41 -0300

    Criando navegação para a página voltar para login ao clicar no botão

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,8 @@
 import { Container, Form } from './styles';
 import { useState } from "react";
 
+import { useNavigate } from 'react-router-dom';
+
 import { api } from "../../services/api";
 
 import logo from '../../assets/logo.svg';
@@ -8,29 +10,31 @@ import logo from '../../assets/logo.svg';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
+const navigate = useNavigate();
 
 export function SignUp(){
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     function handleSignUp(){
         if(!name || !email || !password){
-            return alert("Preencha todos os campos!"); 
+            return alert("Preencha todos os campos!");
         }
 
         api.post("/users", { name, email, password })
         .then(() => {
-            alert("Usuário cadastrado com sucesso!")
+            alert("Usuário cadastrado com sucesso!");
+            navigate("/");
         })
-
         .catch(error => {
             if(error.response){
                 alert(error.response.data.message);
             } else {
                 alert("Não foi possível cadastrar");
             }
-        })
+        });
     }
 
     return (
@@ -65,7 +69,10 @@ export function SignUp(){
                    
                 />
 
-                <Button title="Criar conta" onClick={handleSignUp} />
+                <Button 
+                    title="Criar conta" 
+                    onClick={handleSignUp}
+                />
 
                 <footer>
                     <a href="#">
