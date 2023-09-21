8cf9c2a2b55a88db38ed38425876a61411a16b39
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-21 09:43:18 -0300

    Criando os contextos

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
+import { MyContext } from './myContext';
+
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
@@ -11,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Routes />
+      <MyContext.Provider value={{ name: 'Rodrigo Gonçalves', email: 'rodrigo@email.com' }}>
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>,
 )
