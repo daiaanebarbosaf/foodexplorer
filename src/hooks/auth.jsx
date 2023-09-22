8fd1e83877b7b296cc3f64ade9e95a25d592dfd6
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-22 10:05:39 -0300

    Usando LocalStorange para salvar os dados  da seção

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 import { api } from '../services/api';
 
@@ -11,6 +11,9 @@ function AuthProvider({ children }) {
         try {
             const response = await api.post("/sessions", { email, password });
             const { user, token } = response.data;
+
+            localStorage.setItem("@foodexplorer:user", JSON.stringify(user));
+            localStorage.setItem("@foodexplorer:token", token);
             
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token })
@@ -24,6 +27,20 @@ function AuthProvider({ children }) {
         }
 
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@foodexplorer:token");
+        const user = localStorage.getItem("@foodexplorer:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
 
     return(
         <AuthContext.Provider value={{ signIn, user: data.user }}>
