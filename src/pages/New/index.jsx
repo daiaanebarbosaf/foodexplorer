290c17edfe2baa1b6b6295d64812243256ed965f
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-25 09:00:59 -0300

    Função para remover as TAGS

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -26,6 +26,10 @@ export function New(){
         setNewTag("");
     }
 
+    function handleRemoveTag(deleted){
+        setTags(prevState => prevState.filter(tag => tag !== deleted));
+    }
+
     return (
         <Container>
             <Header/>
@@ -87,7 +91,7 @@ export function New(){
                                 <Ingredients 
                                     key={String(index)}
                                     value={tag}
-                                    onClick={() => { }}
+                                    onClick={() => {handleRemoveTag(tag)}}
                                 />
                             )
                         }
