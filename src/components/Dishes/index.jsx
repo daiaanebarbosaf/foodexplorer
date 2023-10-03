805cf5e1259f5c2ac592bd4305365d0a0cbd247c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-10-03 11:43:57 -0300

    Criando o componente de notas

--- src/components/Dishes/index.jsx
+++ src/components/Dishes/index.jsx
@@ -1,1 +1,41 @@
 import { Container } from './styles';
+
+import unparalleledFlavors from '../../assets/unparalleled-flavors.png';
+import saladRavanello from '../../assets/dishes/saladRavanello.png';
+import spaguettiGambe from '../../assets/dishes/spaguettiGambe.png';
+import prugnaPie from '../../assets/dishes/prugnaPie.png';
+import peachyPastrie from '../../assets/dishes/peachyPastrie.png';
+import espresso from '../../assets/dishes/espresso.png';
+
+import { PiPencilSimpleBold } from 'react-icons/pi';
+import { IoIosArrowForward } from 'react-icons/io';
+import { FiSearch } from 'react-icons/fi';
+
+export function Dish(data, ...rest){
+    return(
+        <Container>
+                <img 
+                    src={saladRavanello} 
+                    alt="Prato de Salada Ravanello" 
+                />
+
+                <p>
+                    {data.title}
+                    <IoIosArrowForward/>
+                </p>
+                <p className="pPrice">R$ 49,97</p>
+                {
+                    data.tags &&
+                    <footer>
+                        {
+                            data.tags.map(tag => {
+                                <span key={tag.id}>
+                                    {tag.name}
+                                </span>
+                            })
+                        }
+                    </footer>
+                }
+        </Container>
+    );
+}
