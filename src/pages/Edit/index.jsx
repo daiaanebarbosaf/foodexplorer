50051257609dc8cd152da6af91e97fd8c8798ed5
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-10-11 10:18:34 -0300

    Adicionando os estados, a função de voltar a página para a Home e a função de remover prato

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,9 +1,11 @@
 import { Container, Form, ImgDishes, SelectCategory } from './styles';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import { useAuth } from '../../hooks/auth';
 
-import { Link } from 'react-router-dom';
+import { Link, useParams, useNavigate  } from 'react-router-dom';
+import { api } from '../../services/api';
 
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
@@ -17,22 +19,41 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { FiUpload } from 'react-icons/fi';
 
 export function Edit(){
-    const { dish, updateDish } = useAuth();
+    const params = useParams();
+    const navigate = useNavigate();
 
-    const [title, setTitle] = useState(dish.title);
-    const [categoty, setCategoty] = useState(dish.categoty);
-    const [tag, setTag] = useState(dish.tag);
-    const [price, setPrice] = useState(dish.price);
-    const [description, setDescription] = useState(dish.description);
+    const { updateDish } = useAuth();
+
+    const [dish, setDish] = useState(null);
+    const [data, setData] = useState([]);
+
+    const [title, setTitle] = useState("");
+    const [categoty, setCategoty] = useState("");
+  
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
 
     const [tags, setTags] = useState([]);
     const [newTag, setNewTag] = useState("");
+
+    function handleBack(){
+        navigate("/");
+    }
+    
+
+    async function handleRemove(){
+        const confirm = window.confirm("Deseja realmente remover o prato?");
+
+        if(confirm) {
+            await api.delete(`/dishes/${params.id}`);
+            handleBack();
+        }
+    }
 
     async function handleUpdate(){
         const dish = {
             title,
             categoty,
-            tag,
             price,
             description,
         }
@@ -41,7 +62,7 @@ export function Edit(){
     }
 
     function handleAddTag(){
-        setTags(prevState => [...prevState, newTag]);
+        setTags((prevState) => [...prevState, newTag]);
         setNewTag("");
     }
 
@@ -49,6 +70,26 @@ export function Edit(){
         setTags(prevState => prevState.filter(tag => tag !== deleted));
     }
 
+    useEffect(() => {
+        async function fetchDish(){
+          const response = await api.get(`/dishes/${params.id}`);
+          setData(response.data);
+        }
+    
+        fetchDish();
+      }, [params.id]);
+
+      useEffect(() => {
+        if (dish) {
+          setTitle(dish.title);
+          setCategoty(dish.categoty);
+          setPrice(dish.price);
+          setDescription(dish.description);
+      
+          const allTags = dish.tags.map((tag) => tag.name);
+          setTags(allTags);
+        }
+      }, [dish]);
 
     return (
         <Container>
@@ -63,99 +104,104 @@ export function Edit(){
                     title="voltar"
                 />
             </Link>
+                <main>
+                    <Form>
+                        <header>
+                            <h1>Editar prato</h1>
+                        </header>
 
-            <main>
-                <Form>
-                    <header>
-                        <h1>Editar prato</h1>
-                    </header>
-
-                    <ImgDishes>
-                        <p>Imagem do prato</p>
-                        <label 
-                            htmlFor="imgDishes"
-                        >
-                            <FiUpload/>
-                            <p>Selecione imagem para alterá-la</p>
-
-                            <input 
-                                id="imgDishes" 
-                                type="file" 
-                            />
-                        </label>
-                    </ImgDishes>
-
-                    <p>Nome</p>
-                    <Input 
-                        placeholder="Salada Ceasar" 
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}
-                    />
-
-                    <SelectCategory
-                        value={categoty}
-                        onChange={e => setCategoty(e.target.value)}
-                    >
-                        <p>Categoria</p>
-                        <div className="input-select">
-                            <select 
+                        <ImgDishes>
+                            <p>Imagem do prato</p>
+                            <label 
+                                htmlFor="imgDishes"
                             >
-                                <option value="1">
-                                    Refeição
-                                </option>
-                                <option value="2">
-                                    Pratos Principais
-                                </option>
+                                <FiUpload/>
+                                <p>Selecione imagem para alterá-la</p>
 
-                            </select>
-                        </div>
-                    </SelectCategory>
-
-                    <p>Ingredientes</p>
-
-                    <div className="tags">
-                    {
-                            tags.map((tag, index) =>
-                                <Ingredients 
-                                    key={String(index)}
-                                    value={tag}
-                                    onClick={() => {handleRemoveTag(tag)}}
+                                <input 
+                                    id="imgDishes" 
+                                    type="file" 
                                 />
-                            )
-                        }
-                        
-                        <Ingredients 
-                            isNew 
-                            placeholder="Adicionar"
-                            onChange={e => setNewTag(e.target.value)}
-                            value={newTag}
-                            onClick={handleAddTag}
+                            </label>
+                        </ImgDishes>
+
+                        <p>Nome</p>
+                        <Input
+                            type="text"
+                            placeholder="Salada Ceasar"
+                            value={data.title} 
+                            onChange={e => setTitle(e.target.value)}
                         />
-                    </div>
 
-                    <p>Preço</p>
-                    <Input 
-                        placeholder="R$ 40,00" 
-                        value={price}
-                        onChange={e => setPrice(e.target.value)}
-                    />
+                        <SelectCategory
+                        >
+                            <p>Categoria</p>
+                            <div className="input-select">
+                                <select 
+                                    value={data.categoty}
+                                    onChange={e => setCategoty(e.target.value)}
+                                >
+                                    <option value="1">
+                                        Refeição
+                                    </option>
+                                    <option value="2">
+                                        Pratos Principais
+                                    </option>
+                                </select>
+                            </div>
+                        </SelectCategory>
 
-                    <p>Descrição</p>
-                    <Textarea 
-                        placeholder="A Salada César é uma opção refrescante para o verão."
-                        value={description}
-                        onChange={e => setDescription(e.target.value)}
-                    />
-                    <div className="buttons">
-                        <Button className="buttonDelete" title="Excluir prato"/> 
-                        <Button 
-                            className="buttonSave" 
-                            title="Salvar alterações"
-                            onClick={handleUpdate}
-                        /> 
-                    </div>
-                </Form>
-            </main>
+                        <p>Ingredientes</p>
+
+                        <div className="tags">
+                            {
+                                tags.map((tag, index) =>
+                                    <Ingredients 
+                                        key={String(index)}
+                                        value={tag}
+                                        onClick={() => {handleRemoveTag(tag)}}
+                                    />
+                                )
+                            }
+                            
+                            <Ingredients 
+                                isnew
+                                placeholder="Adicionar"
+                                onChange={e => setNewTag(e.target.value)}
+                                value={newTag}
+                                onClick={handleAddTag}
+                            />
+                        </div>
+
+                        <p>Preço</p>
+                        <Input 
+                            placeholder="R$ 40,00"
+                            type="number" 
+                            value={data.price}
+                            onChange={e => setPrice(e.target.value)}
+                        />
+
+                        <p>Descrição</p>
+                        <Textarea 
+                            placeholder="A Salada César é uma opção refrescante para o verão."
+                            defaultValue={data.description}
+                            onChange={e => setDescription(e.target.value)}
+                        />
+                        <div className="buttons">
+                            <Button 
+                                className="buttonDelete" 
+                                title="Excluir prato"
+                                onClick={handleRemove}
+                            /> 
+                            <Button 
+                                className="buttonSave" 
+                                title="Salvar alterações"
+                                onClick={handleUpdate}
+                            /> 
+                        </div>
+                    </Form>
+                </main>
+
             <Footer/>
         </Container>
 
