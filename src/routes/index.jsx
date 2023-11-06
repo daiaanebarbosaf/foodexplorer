165925feed6aae7d418dcf5921236e84616511c9
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-06 11:15:52 -0300

    Redirecionando as rotas de acordo com cada usuário.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,31 @@
 import { BrowserRouter } from 'react-router-dom';
-import { useAuth } from '../hooks/auth';
 
-import { AppRoutes } from './app.routes';
+import { useAuth } from '../hooks/auth';
+import { USER_ROLE } from '../utils/roles';
+
+import { AdminRoutes } from './admin.routes';
+import { CustomerRoutes } from './customer.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes(){
     const { user } = useAuth();
+    
+    function AccessRoute(){
+        switch(user.role){
+            case USER_ROLE.ADMIN:
+                return <AdminRoutes/>;
+            
+            case USER_ROLE.CUSTOMER:
+                return <CustomerRoutes/>;
+            
+            default:
+                return <CustomerRoutes/>;
+        }   
+    }
+
     return(
         <BrowserRouter>
-            { user ? <AppRoutes/> : <AuthRoutes />}
+            { user ? <AccessRoute/> : <AuthRoutes />}
         </BrowserRouter>
     )
 }
