5a59c512f858776ee7077a67cdb8af9f2cad45cf
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-06 11:15:29 -0300

    Ciando as rotas que os usuários customers podem acessar

---
+++ src/routes/customer.routes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from  'react-router-dom';
+
+import { Home } from '../pages/Home';
+import { Details } from '../pages/Details';
+import { NotFound } from '../pages/NotFound';
+
+export function CustomerRoutes(){
+    return(
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/details/:id" element={<Details />} />
+
+            <Route path="*" exact={true} element={<NotFound />} />
+        </Routes>
+    )
+}
