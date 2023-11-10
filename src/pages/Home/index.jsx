fce3607b239bd1d59d98b3c59feb6322cbb69298
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-10 10:26:43 -0300

    colocando a página por dentro do main, para definir o padding somente para o conteúdo da página e não interferir no footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -77,105 +77,107 @@ export function Home(){
 
     return(
         <Container>
-            <Header>
-                <Input
-                    icon={FiSearch} 
-                    id="searchPlate" 
-                    placeholder="Busque por pratos ou ingredientes" 
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-            </Header>
-    
-            <Banner className="gradient-container">
-                <img src={unparalleledFlavors} alt="Quitutes ao ar" />
+            <main>
+                <Header>
+                    <Input
+                        icon={FiSearch} 
+                        id="searchPlate" 
+                        placeholder="Busque por pratos ou ingredientes" 
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </Header>
+        
+                <Banner className="gradient-container">
+                    <img src={unparalleledFlavors} alt="Quitutes ao ar" />
 
-                <BannerText>
-                    <h1>Sabores inigualáveis</h1>
-                    <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
-                </BannerText>
+                    <BannerText>
+                        <h1>Sabores inigualáveis</h1>
+                        <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
+                    </BannerText>
+                    
+                </Banner>
                 
-            </Banner>
+                <Profile>
+                    <span>Olá, <strong>{user.name}</strong> </span>
+                    <p>Perfil de {user.role} </p>
+                </Profile>
+
+                <Section title="Refeições">
+
+                    <swiper-container
+                    
+                            slides-per-view="auto"
+                            loop="true"
+                            grab-cursor="true"
+                        >
+                            <div className="dishes"> 
+                    
+                                {!dishes ? <p>Sem resultados</p> : ""}                
+                                {
+                                    dishes && dishes.map((dish, categoty) => (
+                                        <swiper-slide key={String(dish.id)}>
+                                            <Dishes
+                                                imgdish={`${api.defaults.baseURL}/files/${dish.imgdish}`}
+                                                data={dish}
+                                                dishId={dish.id}
+                                                onClick={() => handleDetails(dish.id)} 
+                                                categoty={categoty}
+                                            />
+                                        </swiper-slide>                                
+                                    ))
+                                }
+
+                            </div>
+
+                    </swiper-container>
+                    
+                </Section>
+
+                <Section title="Sobremesas">
+
+                <div className="dish"> 
+                    
+                    {!dessert ? <p>Sem resultados</p> : ""}                
+                    {
+                        dessert && dessert.map((dessert) => (
+                                <Dishes
+                                    key={String(dessert.id)}
+                                    imgdish={`${api.defaults.baseURL}/files/${dessert.imgdish}`}
+                                    data={dessert}
+                                    dishId={dessert.id}
+                                    onClick={() => handleDetails(dessert.id)} 
+                            />
+                            
+                        ))
+                    }
+                    
+                </div>
+
+                </Section>
+
+                <Section title="Bebidas">
+
+                <div className="dish"> 
+                    
+                    {!drink ? <p>Sem resultados</p> : ""}                
+                    {
+                        drink && drink.map((drink) => (
+                                <Dishes
+                                    key={String(drink.id)}
+                                    imgdish={`${api.defaults.baseURL}/files/${drink.imgdish}`}
+                                    data={drink}
+                                    dishId={drink.id}
+                                    onClick={() => handleDetails(drink.id)} 
+                            />
+                            
+                        ))
+                    }
+                    
+                </div>
+
+                </Section>
+            </main>
             
-            <Profile>
-                <span>Olá, <strong>{user.name}</strong> </span>
-                <p>Perfil de {user.role} </p>
-            </Profile>
-
-            <Section title="Refeições">
-
-                <swiper-container
-                
-                        slides-per-view="auto"
-                        loop="true"
-                        grab-cursor="true"
-                    >
-                        <div className="dishes"> 
-                
-                            {!dishes ? <p>Sem resultados</p> : ""}                
-                            {
-                                dishes && dishes.map((dish, categoty) => (
-                                    <swiper-slide key={String(dish.id)}>
-                                        <Dishes
-                                            imgdish={`${api.defaults.baseURL}/files/${dish.imgdish}`}
-                                            data={dish}
-                                            dishId={dish.id}
-                                            onClick={() => handleDetails(dish.id)} 
-                                            categoty={categoty}
-                                        />
-                                    </swiper-slide>                                
-                                ))
-                            }
-
-                        </div>
-
-                </swiper-container>
-                
-            </Section>
-
-            <Section title="Sobremesas">
-
-            <div className="dish"> 
-                
-                {!dessert ? <p>Sem resultados</p> : ""}                
-                {
-                    dessert && dessert.map((dessert) => (
-                            <Dishes
-                                key={String(dessert.id)}
-                                imgdish={`${api.defaults.baseURL}/files/${dessert.imgdish}`}
-                                data={dessert}
-                                dishId={dessert.id}
-                                onClick={() => handleDetails(dessert.id)} 
-                        />
-                        
-                    ))
-                }
-                
-            </div>
-
-            </Section>
-
-            <Section title="Bebidas">
-
-            <div className="dish"> 
-                
-                {!drink ? <p>Sem resultados</p> : ""}                
-                {
-                    drink && drink.map((drink) => (
-                            <Dishes
-                                key={String(drink.id)}
-                                imgdish={`${api.defaults.baseURL}/files/${drink.imgdish}`}
-                                data={drink}
-                                dishId={drink.id}
-                                onClick={() => handleDetails(drink.id)} 
-                        />
-                        
-                    ))
-                }
-                
-            </div>
-
-            </Section>
-
             <Footer />
 
         </Container>
