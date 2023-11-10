fa9a947d03c473e3c491b7a569653654715cad6c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-10 09:48:19 -0300

    Style de imagens no botão

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
     width: 100%;
     background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
     color: ${({ theme }) => theme.COLORS.LIGHT_100};
@@ -11,7 +16,13 @@ export const Container = styled.button`
     border-radius: 5px;
 
     font-family: 'Poppins', sans-serif;
-    font-weight: 500;
+    font-weight: 400;
+    font-size: 0.7rem;
+
+    > svg {
+        width: 1.4rem;
+        height: 1.4rem;
+    }
 
     &:disabled{
         opacity: 0.5;
