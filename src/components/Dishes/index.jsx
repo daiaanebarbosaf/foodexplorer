a94dd9d4e183a3b4efc7cfb9c0febd54db18d0f1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-10 09:48:22 -0300

    Update index.jsx

--- src/components/Dishes/index.jsx
+++ src/components/Dishes/index.jsx
@@ -42,7 +42,6 @@ export function Dishes({ data, dishId, imgdish, ...rest }){
                 }
                 
                 
-
             </a>
             
             <Content {...rest}>
