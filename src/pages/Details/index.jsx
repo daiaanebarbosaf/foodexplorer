3aa2d0e6c593fc66042d14f7d7b667f6208036aa
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-16 10:18:59 -0300

    Refatorando a página para controlar melhor como será a distribuição na versão Desktop

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -30,7 +30,8 @@ export function Details(){
   const navigate = useNavigate();
 
   function handleBack(){
-    navigate(-1);
+    navigate("/");
+
   }
 
   function handleEdit(){
@@ -63,66 +64,65 @@ export function Details(){
         {
           data &&
               <main>
+                <img  
+                  src={`${api.defaults.baseURL}/files/${data.imgdish}`} 
+                  alt="Image in the Dish" 
+                />
                   <div className="dishDescription">
-                    <div className="col-a">
-                      <img 
-                          src={`${api.defaults.baseURL}/files/${data.imgdish}`} 
-                          alt="Prato de Salada Ravanello" 
-                          
-                        />
-                    </div>
-
-                      <div className="titleAndDescription col-b">
-                        <h1>
-                          {data.title}
-                        </h1>
-                                      
-                        <p >
-                          {data.description}
-                        </p>
+ 
+                      <div className="titleAndDescription ">
+                          <h1>
+                            {data.title}
+                          </h1>
+                                        
+                          <p >
+                            {data.description}
+                          </p>
                       </div>
 
-                    </div>
-
-                    {
-                      data.tags &&
-                      <div className="tags col-b">
+                      <div className="tagsAndButtons">
                         {
-                          data.tags.map(tag => (
-                            <Tag 
-                              key={String(tag.id)}
-                              title={tag.name}
-                            />
+                            data.tags &&
+                            <div className="tags">
+                              {
+                                data.tags.map(tag => (
+                                  <Tag 
+                                    key={String(tag.id)}
+                                    title={tag.name}
+                                  />
 
-                          ))
+                                ))
+                              }
+                            </div>
+                            
+                          }
+                      <div className="buttonsDetails">
+                        {
+                          [USER_ROLE.ADMIN].includes(user.role) &&
+                          <Button
+                            className="buttonEditDish"
+                            title="Editar prato"
+                            onClick={handleEdit}
+                          />
+                        }
+
+                        {
+                          [USER_ROLE.CUSTOMER].includes(user.role) &&
+                          <>
+                            <div className="orderInformation">
+                              <NumberOfDishes number="01"/>
+                                <Button
+                                  className="buttoneOrderDish"
+                                  title={`inclur ∙ R$ ${data.price}`}
+                                  icon={PiReceipt}
+                                  onClick={handleEdit}
+                                />
+                            </div>
+                          </>
                         }
                       </div>
-                    }
-                  
-                  <div className="col-b">
-                    {
-                        [USER_ROLE.ADMIN].includes(user.role) &&
-                        <Button
-                          className="buttonEditDish"
-                          title="Editar prato"
-                          onClick={handleEdit}
-                        />
-                      }  
-
-                      {
-                        [USER_ROLE.CUSTOMER].includes(user.role) &&
-                        <>
-                          <div className="orderInformation">
-                            <NumberOfDishes number="01"/>
-                              <Button
-                                className="buttoneOrderDish"
-                                title={`pedir ∙ R$ ${data.price}`}
-                                icon={PiReceipt}
-                                onClick={handleEdit}
-                              />
-                          </div>
-                        </>
-                      }       
+                  </div>
+  
                   </div>
                                   
               </main>
