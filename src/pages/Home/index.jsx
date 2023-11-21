6761482f785936a293f44e89b60f5afc951e16ce
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-21 10:20:30 -0300

    Adiconando o Swiper nas outras seções

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -40,14 +40,31 @@ export function Home(){
     const [dessert, setDessert] = useState([]);
     const [drink, setDrink] = useState([]);
 
-    const [width, setWidth] = useState(0);
-
     const navigate = useNavigate();
     const params = useParams();
 
     const { user } = useAuth();
 
-    const [slidePerView, setSlidePerView] = useState();
+    const [slidePerView, setSlidePerView] = useState(3);
+
+    useEffect(() => {
+
+        function handleResize(){
+            if(window.innerWidth < 768){
+                setSlidePerView(1)
+            } else {
+                setSlidePerView(3);
+            } 
+        }
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        return() => {
+            window.addEventListener("resize", handleResize);
+        }
+    }, [])
 
     function handleDetails(id){
         navigate(`/details/${id}`);
@@ -88,25 +105,6 @@ export function Home(){
 
     const carousel = useRef();
 
-    useEffect(() => {
-
-        function handleResize(){
-            if(window.innerWidth > 720){
-                setSlidePerView(3)
-            } else {
-                setSlidePerView(1);
-            } 
-        }
-
-        handleResize();
-
-        window.addEventListener("resize", handleResize);
-
-        return() => {
-            window.addEventListener("resize", handleResize);
-        }
-    }, [])
-
     return(
         <Container>
             <main>
@@ -141,14 +139,18 @@ export function Home(){
                     <Swiper 
                         className="dishes"
                         slidesPerView={slidePerView}
-                        paginatison={{clickable: true}}
-        
-                        navigation                        
+                        pagination={{clickable: true}}
+                        navigation 
+                        
+                        data-swiper-parallax-x="-10"                       
                     >
                         {!dishes ? <p>Sem resultados</p> : ""} 
                         {
                             dishes && dishes.map((dish, categoty) => (
-                                <SwiperSlide key={String(dish.id)} space-between="30">
+                                <SwiperSlide 
+                                    key={String(dish.id)}
+                                    
+                                >
                                     <Dishes
                                         className="dish"
                                         
@@ -169,68 +171,58 @@ export function Home(){
 
                 <Section title="Sobremesas">
 
-                    <motion.div                         
-                            ref={carousel}
-                            className="carousel"
-                            whileTap={{cursor: "grabbing"}}> 
-                        <motion.div 
-                            className="dishes"
-                            drag="x"
-                            dragConstraints={{right: 0, left: -500}}
-                            initial={{ x: 100}}
-                            animate={{ x: 0}}
-                            transition={{duration: 0.8}}
-                        >
-                            {!dessert ? <p>Sem resultados</p> : ""}                
-                            {
-                                dessert && dessert.map((dessert) => (
+                    <Swiper                        
+                        className="dishes "
+                        slidesPerView={slidePerView}
+                        pagination={{clickable: true}}
+                        navigation
+                        
+                    > 
+                        {!dessert ? <p>Sem resultados</p> : ""} 
+                        {
+                            dessert && dessert.map((dessert) => (
+                                <SwiperSlide key={String(dessert.id)}>
                                         <Dishes
-                                            key={String(dessert.id)}
+                                            
                                             imgdish={`${api.defaults.baseURL}/files/${dessert.imgdish}`}
                                             data={dessert}
                                             dishId={dessert.id}
                                             onClick={() => handleDetails(dessert.id)} 
-                                    />
-                                    
-                                ))
-                            }
-                        
-                        </motion.div>
-                    </motion.div>
+                                        />
+                                </SwiperSlide>
+                                
+                            ))
+                        }
+      
+                    </Swiper>
 
                 </Section>
 
-                <Section title="Bebidas">       
+                <Section title="Bebidas">
 
-                <motion.div 
-                    ref={carousel}
-                    className="carousel"
-                    whileTap={{cursor: "grabbing"}}
-                > 
-                    <motion.div 
-                        className="dishes"
-                        drag="x"
-                        dragConstraints={{right: 0, left: -500}}
-                        initial={{ x: 100}}
-                        animate={{ x: 0}}
-                        transition={{duration: 0.8}}
-                        >
-                            {!drink ? <p>Sem resultados</p> : ""}                
+                    <Swiper
+                        className="dishes "
+                        slidesPerView={slidePerView}
+                        pagination={{clickable: true}}
+                        navigation
+                    >
+                        
+                        {!drink ? <p>Sem resultados</p> : ""}   
                             {
                                 drink && drink.map((drink) => (
+                                    <SwiperSlide>
                                         <Dishes
                                             key={String(drink.id)}
                                             imgdish={`${api.defaults.baseURL}/files/${drink.imgdish}`}
                                             data={drink}
                                             dishId={drink.id}
                                             onClick={() => handleDetails(drink.id)} 
-                                    />
-                                    
+                                        />
+                                    </SwiperSlide>                                          
                                 ))
                             }
-                        </motion.div>
-                    
-                </motion.div>
+                            
+                    </Swiper>       
 
                 </Section>
             </main>
