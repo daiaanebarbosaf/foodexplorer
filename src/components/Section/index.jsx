870aac943be6c6e6097e376beed89152a8160c08
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-11-22 17:09:09 -0300

    refatoração

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -4,7 +4,7 @@ export function Section({ title, children }){
     return (
         <Container>
             <h2>{title}</h2>
-            {children}
+            <main>{children}</main>
         </Container>
     );
 }
